b6111b1994165b6cb4bbd68b9eb2f4529f2ae32d
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-03-25 12:28:11 +0100

    update app activity

--- app/src/main/java/com/app/artisanpay/AppActivity.kt
+++ app/src/main/java/com/app/artisanpay/AppActivity.kt
@@ -1,12 +1,16 @@
 package com.app.artisanpay
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_app.*
 
 class AppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+
+        message.text=getString(R.string.message)
     }
 }
